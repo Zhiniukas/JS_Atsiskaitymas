0eec7d3c8f750691f3033cdf53f2088a710f2b13
Zhiniukas <zhiniukas@gmail.com>
2022-10-27 14:04:07 +0300

    Task 10 completed

---
+++ Task 3/createList.js
@@ -0,0 +1,53 @@
+import(users) from "./script.js"
+
+
+
+const populateTable = (robots, tbodyElement) => {
+    robots.forEach((robot) => {
+        addRow(robot, tbodyElement);
+    });
+};
+
+
+
+const createTable = (list) => {
+    const userList = document.getElementById("output");
+
+
+    const userListElement = createListItem(user.login, user.avatar_url)
+
+
+
+    const theadElement = document.createElement("thead");
+
+    const tbodyElement = document.createElement("tbody");
+
+    const idHeader = createElementWithParams("th", { innerText: "ID" });
+    const imgHeader = createElementWithParams("th", { innerText: "Image" });
+    const firstNameHeader = createElementWithParams("th", {
+        innerText: "User",
+    });
+    const lastNameHeader = createElementWithParams("th", {
+        innerText: "Avatar url",
+    });
+    const cityHeader = createElementWithParams("th", { innerText: "City" });
+    const favColorHeader = createElementWithParams("th", {
+        innerText: "Favorite color",
+    });
+
+    theadElement.append(
+        idHeader,
+        imgHeader,
+        firstNameHeader,
+        lastNameHeader,
+        cityHeader,
+        favColorHeader
+    );
+
+    populateTable(robots, tbodyElement);
+
+    tableElement.append(theadElement, tbodyElement);
+    document.body.append(tableElement);
+};
+
+export { createTable, createElementWithParams, populateTable };
