700785c225450004c3b6759cf00696c6d96e5a2e
Zhiniukas <zhiniukas@gmail.com>
2022-10-28 14:34:44 +0300

    Task 10 assignment to top

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,12 +1,12 @@
+/* ------------------------------ TASK 10 ---------------------------------------------------
+Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
+-------------------------------------------------------------------------------------------- */
 import { composition } from "./modules/math/composition.js";
 import { division } from "./modules/math/division.js";
 import { substraction } from "./modules/math/subtraction.js";
 import { multiplication } from "./modules/math/multiplication.js";
 import { one, two, three, four, five } from "./modules/numbers/numbers.js";
 
-/* ------------------------------ TASK 10 ---------------------------------------------------
-Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funkcijos.
--------------------------------------------------------------------------------------------- */
 
 let a = composition(one, four);
 let b = division(four, two);
